7715223092951b90723b3c61efd2dce9ab7d2c3b
kvark85 <kvark1985@gmail.com>
2017-06-21 23:55:01 +0300

    Added body-parser.

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const config = require('./config/server.config');
 const http = require('http');
 const app = require('express')();
+const bodyParser = require('body-parser');
 
 // Routes import
 const index = require('./routes/index.router');
 
 const logger = require('./utilities/logger');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 // Middleware register
 app.use(logger);
 
-app.use('/', index);
+app.use((req, res, next) => {
+    console.log(JSON.stringify(req.body));
+    next();
+});
 
+app.use('/', (req, res)=>{
+    res.sendfile('testForm.html');
+});
 
 const server = http.createServer(app);
 
