65d46efca3c1f0806e70393db2dd850fb32745a8
kvark85 <kvark1985@gmail.com>
2017-06-21 23:32:49 +0300

    Added cookie-parser.

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const config = require('./config/server.config');
 const http = require('http');
 const app = require('express')();
+const cookieParser = require('cookie-parser');
 
 // Routes import
 const index = require('./routes/index.router');
 
 const logger = require('./utilities/logger');
 
+app.use(cookieParser());
+
 // Middleware register
 app.use(logger);
+
+// Middleware for testing cookie-parser
+app.use((req, res, next)=> {
+    console.log('Cookies: ', req.cookies);
+    next();
+});
 
 app.use('/', index);
 
