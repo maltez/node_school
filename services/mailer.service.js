95687c30cf886976d05845e4ad8cf3e16cee3fd8
sholast <kozelalexey@gmail.com>
2017-06-22 11:18:14 +0300

    nodemailer service and config

---
+++ services/mailer.service.js
@@ -0,0 +1,22 @@
+const nodeMailer = require('nodemailer');
+const mailerConfig = require('../config/mailer.config');
+
+class MailerService {
+    constructor(mailerConfig) {
+        this.transporter = nodeMailer.createTransport(mailerConfig);
+    }
+
+    sendMail(mailOptions) {
+        return new Promise((resolve, reject) => {
+            return this.transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    reject(new Error(error));
+                    return;
+                }
+                resolve(info);
+            });
+        })
+    }
+}
+
+module.exports = new MailerService(mailerConfig);
